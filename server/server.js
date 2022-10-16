c6d4ce81eaced2b445921f039074e4eb81c63e4c
orcaetal <57076114+orcaetal@users.noreply.github.com>
2022-10-15 20:56:01 -0600

    dotenv from react

--- server/server.js
+++ server/server.js
@@ -1,42 +1,44 @@
 const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const app = express();
 
 const fileStorageEngine = multer.diskStorage({
     destination: (req, file, callback) => {
-        callback(null,'../client/src/assets')
+        callback(null,'../client/src/assets');
     },
     filename: (req, file, callback) => {
-        callback(null, file.originalname)
+        callback(null, file.originalname);
     }
-});
+})
 
 const upload = multer({storage: fileStorageEngine});
 
 app.use(cors());
-app.use(express.json());                           /* allows JSON Objects to be posted */
-app.use(express.urlencoded({ extended: true }));   /* allows JSON Objects with strings and arrays*/
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 require('./config/mongoose.config');
 require('./routes/need.routes')(app);
 require('./routes/story.routes')(app);
-require("dotenv").config()
-const stripe = require("stripe")(process.env.STRIPE_SECRET)
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //handle file upload
 app.post('/api/single', upload.single('image'), (req, res) => {
-    console.log(req.file);
     res.send('File upload succesful');
+})
+
+//handle dotenv key request
+app.get('/api/admin', (req, res) => {
+    res.send(process.env.ADMIN_SECRET);
 })
 
 //handle payment through stripe
 app.post("/api/payment", cors(), async (req, res) => {
-	console.log("body",req.body)
-	let { amount, id, description, receipt_email } = req.body
+	let { amount, id, description, receipt_email } = req.body;
 	try {
 		const payment = await stripe.paymentIntents.create({
 			amount,
@@ -46,13 +48,11 @@ app.post("/api/payment", cors(), async (req, res) => {
 			confirm: true,
 			receipt_email
 		})
-		console.log("Payment", payment)
 		res.json({
 			message: "Payment successful",
 			success: true
 		})
 	} catch (error) {
-		console.log("Error", error)
 		res.json({
 			message: "Payment failed",
 			success: false
@@ -61,6 +61,6 @@ app.post("/api/payment", cors(), async (req, res) => {
 })
 
 app.listen(8000, () => {
-    console.log("Listening at Port 8000")
+    console.log("Listening at Port 8000");
 })
 
