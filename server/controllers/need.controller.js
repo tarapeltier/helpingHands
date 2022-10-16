2bc4c10da3b0414309baeb9b3846394e5c1387f0
orcaetal <57076114+orcaetal@users.noreply.github.com>
2022-10-15 20:56:46 -0600

    Add files via upload

--- server/controllers/need.controller.js
+++ server/controllers/need.controller.js
@@ -9,32 +9,26 @@ module.exports.createNeed = (request, response) => {
 
 module.exports.getAllNeeds = (request, response) => {
     Need.find({})
-        .then(need => {
-            console.log(need); //console logs are optional, but they are highly recommended for troubleshooting!
-            response.json(need);
-        })
-        .catch(err => {
-            console.log(err);
-            response.json(err);
-        })
+        .then(need => response.json(need))
+        .catch(err => response.json(err));
 }
 
 module.exports.getNeed = (request, response) => {
     Need.findOne({_id:request.params.id})
         .then(need => response.json(need))
-        .catch(err => response.json(err))
+        .catch(err => response.json(err));
 }
 
 module.exports.updateNeed = (request, response) => {
     Need.findOneAndUpdate({_id: request.params.id}, request.body, {new:true})
         .then(updatedNeed => response.json(updatedNeed))
-        .catch(err => response.json(err))
+        .catch(err => response.json(err));
 }
 
 module.exports.deleteNeed = (request, response) => {
     Need.deleteOne({ _id: request.params.id }) 
         .then(deleteConfirmation => response.json(deleteConfirmation))
-        .catch(err => response.json(err))
+        .catch(err => response.json(err));
 }
 
 
